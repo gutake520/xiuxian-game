7162b4bd75edf53820997aa112d5d69c983ff03f
gutake520 <gutake0102@gmail.com>
2026-09-25 21:00:43 +0800

    更新陆仁嘉家仇剧情：systems/boss-line.js

--- systems/boss-line.js
+++ systems/boss-line.js
@@ -1,9 +1,13 @@
 import {realmProgress} from '../data/realms.js';
 import {equipmentStats} from './inventory.js';
-import {libraryElderName} from './sect-library.js';
+import {LIBRARY_ELDERS} from './sect-library.js';
 
 const round2=value=>Math.round((value+Number.EPSILON)*100)/100;
-export const BOSS_NAME='仇人';
+export const BOSS_NAME='陆仁嘉';
+export const bossRescuerName=save=>{
+ const name=LIBRARY_ELDERS[save.player.sect]?.name;
+ return name?`${name}师叔`:'藏书阁前辈';
+};
 
 export function ensureBossLine(save){
  if(!save.bossLine&&realmProgress(save.player).index===9)save.bossLine={phase:'ambush'};
@@ -18,7 +22,7 @@ export function bossAttributes(save,multiplier){
 export function resolveBossAmbush(save){
  if(save.battle)throw new Error('请先结束当前战斗。');
  if(save.bossLine?.phase!=='ambush')throw new Error('这段往事已经结束。');
- const enemy=bossAttributes(save,2),elder=libraryElderName(save);
+ const enemy=bossAttributes(save,2),elder=bossRescuerName(save);
  save.player.hp=round2(Math.min(save.player.hp,5));
  save.bossLine={phase:'wounded',rescuePending:true,elder,firstFight:enemy};
  return `${BOSS_NAME}将你击倒。${elder}赶来救下你，重伤了${BOSS_NAME}；对方逃入丰原镇附近的山中养伤。`;
@@ -27,5 +31,5 @@ export function resolveBossAmbush(save){
 export function acknowledgeBossRescue(save){
  if(save.bossLine?.phase!=='wounded'||!save.bossLine.rescuePending)throw new Error('此事已经记下。');
  save.bossLine.rescuePending=false;
- return '丰原镇出现一座陌生山峰，仇人正在山中养伤。';
+ return `丰原镇出现一座陌生山峰，${BOSS_NAME}正在山中养伤。`;
 }
